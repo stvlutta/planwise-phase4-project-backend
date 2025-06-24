9facdab79e0d6d63be2b18aeee99ae1bfb82ee3e
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-06-24 12:23:05 +0300

    Add revision identifiers to initial migration file

--- server/migrations/versions/6437ca4377c0_initial_migration_with_user_tas  k_.py
+++ server/migrations/versions/6437ca4377c0_initial_migration_with_user_tas  k_.py
@@ -4,6 +4,12 @@ Revision ID: 6437ca4377c0
 Revises: 
 Create Date: 2025-06-23 00:19:44.607360
 
+# revision identifiers, used by Alembic.
+revision = '6437ca4377c0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
 """
 from alembic import op
 import sqlalchemy as sa
