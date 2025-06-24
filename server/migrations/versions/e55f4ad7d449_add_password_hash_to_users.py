7c318497bca3b9a3e384ee908717d39c19e0e66c
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-06-24 12:29:45 +0300

    Add revision identifiers to add_password_hash_to_users migration file

--- server/migrations/versions/e55f4ad7d449_add_password_hash_to_users.py
+++ server/migrations/versions/e55f4ad7d449_add_password_hash_to_users.py
@@ -4,6 +4,12 @@ Revision ID: e55f4ad7d449
 Revises: 6437ca4377c0
 Create Date: 2025-06-23 00:45:44.722482
 
+# revision identifiers, used by Alembic.
+revision = 'e55f4ad7d449'
+down_revision = '6437ca4377c0'
+branch_labels = None
+depends_on = None
+
 """
 from alembic import op
 import sqlalchemy as sa 
