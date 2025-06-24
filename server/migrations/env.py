107f1c4844ea3b0f6a0ab15335a8c218e7dd5f56
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-06-24 12:17:24 +0300

    Add run_migrations_offline function to env.py

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -41,3 +41,23 @@ def get_metadata():
     if hasattr(target_db, 'metadatas'):
         return target_db.metadatas[None]
     return target_db.metadata
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=get_metadata(), literal_binds=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
