a0f5dbaa86fed8088ea593d4021cc094e1739ab7
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-06-24 12:16:48 +0300

    Add target_metadata and config.set_main_option logic to env.py

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -29,3 +29,10 @@ def get_engine_url():
             '%', '%%')
     except AttributeError:
         return str(get_engine().url).replace('%', '%%')
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+config.set_main_option('sqlalchemy.url', get_engine_url())
+target_db = current_app.extensions['migrate'].db
