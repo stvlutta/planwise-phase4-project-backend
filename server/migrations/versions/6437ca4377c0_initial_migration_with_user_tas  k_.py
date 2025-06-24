1103390c3e2d01bc01aa5cf05a23167d0fb96f9f
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-06-24 12:22:50 +0300

    Add Alembic header and import statements to initial migration file

---
+++ server/migrations/versions/6437ca4377c0_initial_migration_with_user_tas  k_.py
@@ -0,0 +1,9 @@
+"""Initial migration with User, Task, Project, and ProjectCollaborator models
+
+Revision ID: 6437ca4377c0
+Revises: 
+Create Date: 2025-06-23 00:19:44.607360
+
+"""
+from alembic import op
+import sqlalchemy as sa
