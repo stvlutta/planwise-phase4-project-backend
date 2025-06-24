97ee1b1e0eeb9251760a4dd08f674395cb926bab
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-06-24 12:16:27 +0300

    Add get_engine and get_engine_url functions to env.py

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -13,3 +13,19 @@ config = context.config
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
+
+def get_engine():
+    try:
+        # this works with Flask-SQLAlchemy<3 and Alchemical
+        return current_app.extensions['migrate'].db.get_engine()
+    except (TypeError, AttributeError):
+        # this works with Flask-SQLAlchemy>=3
+        return current_app.extensions['migrate'].db.engine
+
+
+def get_engine_url():
+    try:
+        return get_engine().url.render_as_string(hide_password=False).replace(
+            '%', '%%')
+    except AttributeError:
+        return str(get_engine().url).replace('%', '%%')
